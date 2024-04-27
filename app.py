a959734464fba91b027044a0eb31386bd40f4da9
gky <1142262478@qq.com>
2024-04-27 15:32:45 +0800

    接口按id查古诗词

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 # app.py
-
 import sys
+from flask import Flask, jsonify, request
+from config import Config
+from models import db, _get_one_random_poem, _get_poem_by_id, _get_all_poems
+from datetime import datetime
+
 sys.path.extend([r"/data/env/studyPoem-server/studyPoem-server"])
 
 
-from flask import Flask, jsonify
-from config import Config
-from models import db, get_one_random_poem
-from datetime import datetime
-
 app = Flask(__name__)
 app.config.from_object(Config)  # 加载配置
-
 db.init_app(app)
 
 
@@ -20,12 +18,39 @@ def helloworld():
     return f'hello-world {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
 
 
-# 定义GET请求接口来获取所有poem
 @app.route('/studypoem/random_poem', methods=['GET'])
 def get_random_poem():
-    _poem = get_one_random_poem()
-    poem = {'id': _poem.id, 'title': _poem.title, 'author': _poem.author, 'content': _poem.content, 'yiwen': _poem.yiwen,
-         'zhailu': _poem.zhailu}
+    """
+    随机返回一首古诗
+    :return:
+    """
+    poem = _get_one_random_poem()
+    return jsonify(poem)
+
+
+@app.route('/studypoem/all_poems', methods=['GET'])
+def get_all_poems():
+    """
+    返回所有古诗
+    :return:
+    """
+    poems = _get_all_poems()
+    return jsonify(poems)
+
+
+@app.route('/studypoem/poem_details', methods=['GET'])
+def _get_poem_details():
+    """
+    根据id返回古诗详情
+    http://127.0.0.1:5000/studypoem/poem_details?id=af4715c0208f
+    :return:
+    """
+    # 获取查询参数 'id'
+    poem_id = request.args.get('id')
+    # 调用函数获取诗词详情
+    poem = _get_poem_by_id(poem_id)
+    if poem is None:
+        return jsonify({'error': 'Poem not found!'}), 404
     return jsonify(poem)
 
 
--- models.py
+++ models.py
@@ -13,9 +13,43 @@ class Poems(db.Model):
     yiwen = db.Column(db.Text)
     zhailu = db.Column(db.Text)
 
-def get_one_random_poem():
-    poem = Poems.query.order_by(func.rand()).first()
-    return poem
 
-def get_all_poems():
-    return Poems.query.all()
+# Poems类 转json
+def poem_to_dict(poem):
+    return {
+        'id': poem.id,
+        'title': poem.title,
+        'author': poem.author,
+        'content': poem.content,
+        'yiwen': poem.yiwen,
+        'zhailu': poem.zhailu
+    }
+
+
+def _get_one_random_poem():
+    """
+    随机返回一首古诗
+    :return:
+    """
+    poem = Poems.query.order_by(func.rand()).first()
+    return poem_to_dict(poem)
+
+
+def _get_poem_by_id(poem_id):
+    """
+    根据id查询古诗
+    :param poem_id:
+    :return:
+    """
+    poem = Poems.query.get(poem_id)
+    return poem_to_dict(poem)
+
+
+def _get_all_poems():
+    """
+    返回所有古诗
+    :return:
+    """
+    poems = Poems.query.all()
+    poems_list = [poem_to_dict(poem) for poem in poems]
+    return poems_list
