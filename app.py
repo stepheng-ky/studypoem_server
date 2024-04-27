642baf34bd556bd3708aae9a2d05e96c4f9e1a78
gky <1142262478@qq.com>
2024-04-27 22:59:07 +0800

    category

--- app.py
+++ app.py
@@ -2,11 +2,10 @@
 import sys
 from flask import Flask, jsonify, request
 from config import Config
-from models import db, _get_one_random_poem, _get_poem_by_id, _get_all_poems
+from models import db, _get_one_random_poem, _get_poem_by_id, _get_all_poems, _get_all_categories,_get_poems_by_category_id
 from datetime import datetime
 
 sys.path.extend([r"/data/env/studyPoem-server/studypoem_server"])
-
 
 app = Flask(__name__)
 app.config.from_object(Config)  # 加载配置
@@ -39,7 +38,7 @@ def get_all_poems():
 
 
 @app.route('/studypoem/poem_details', methods=['GET'])
-def _get_poem_details():
+def get_poem_by_id():
     """
     根据id返回古诗详情
     test：http://127.0.0.1:5000/studypoem/poem_details?id=af4715c0208f
@@ -50,8 +49,33 @@ def _get_poem_details():
     # 调用函数获取诗词详情
     poem = _get_poem_by_id(poem_id)
     if poem is None:
-        return jsonify({'error': 'Poem not found!'}), 404
+        return jsonify({'error': '古诗 not found!'}), 404
     return jsonify(poem)
+
+
+@app.route('/studypoem/all_categories', methods=['GET'])
+def get_all_categories():
+    """
+    返回所有古诗类别
+    :return:
+    """
+    categories = _get_all_categories()
+    return jsonify(categories)
+
+
+@app.route('/studypoem/category', methods=['GET'])
+def get_poems_by_category_id():
+    """
+    根据类别id返回古诗s
+    :return:
+    """
+    # 获取查询参数 'id'
+    category_id = request.args.get('category_id')
+    # 调用函数获取诗词详情
+    category = _get_poems_by_category_id(category_id)
+    if category is None:
+        return jsonify({'error': '类别 not found!'}), 404
+    return jsonify(category)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -6,24 +6,44 @@ db = SQLAlchemy()
 
 
 class Poems(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    author = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.String(255), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text)
     yiwen = db.Column(db.Text)
     zhailu = db.Column(db.Text)
+    author_short = db.Column(db.String(100), nullable=False)
 
 
-# Poems类 转json
-def poem_to_dict(poem):
-    return {
-        'id': poem.id,
-        'title': poem.title,
-        'author': poem.author,
-        'content': poem.content,
-        'yiwen': poem.yiwen,
-        'zhailu': poem.zhailu
-    }
+class Categories(db.Model):
+    category_id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(255), nullable=False)
+
+
+class CategoryPoem(db.Model):
+    category_id = db.Column(db.Integer, primary_key=True)
+    second_level_category = db.Column(db.String(255))
+    id = db.Column(db.String(255), primary_key=True)
+
+
+def model_to_dict(model):
+    """
+    将Flask-SQLAlchemy模型对象转换为字典。
+    """
+    # 初始化一个空字典来存储属性
+    if not model:
+        return
+    result = {}
+    # 遍历模型对象的所有属性
+    for column in model.__table__.columns:
+        # 使用getattr来获取属性的值
+        value = getattr(model, column.name)
+        # 如果值是关系对象（比如另一个模型），则递归调用model_to_dict
+        if isinstance(value, db.Model):
+            value = model_to_dict(value)
+        # 将属性名和值添加到字典中
+        result[column.name] = value
+    return result
 
 
 def _get_one_random_poem():
@@ -31,8 +51,8 @@ def _get_one_random_poem():
     随机返回一首古诗
     :return:
     """
-    poem = Poems.query.order_by(func.rand()).first()
-    return poem_to_dict(poem)
+    poem = model_to_dict(Poems.query.order_by(func.rand()).first())
+    return poem
 
 
 def _get_poem_by_id(poem_id):
@@ -41,8 +61,8 @@ def _get_poem_by_id(poem_id):
     :param poem_id:
     :return:
     """
-    poem = Poems.query.get(poem_id)
-    return poem_to_dict(poem)
+    poem = model_to_dict(Poems.query.get(poem_id))
+    return poem
 
 
 def _get_all_poems():
@@ -51,5 +71,39 @@ def _get_all_poems():
     :return:
     """
     poems = Poems.query.all()
-    poems_list = [poem_to_dict(poem) for poem in poems]
+    poems_list = [model_to_dict(poem) for poem in poems]
     return poems_list
+
+
+def _get_all_categories():
+    """
+    返回所有类别
+    :return:
+    """
+    categories = Categories.query.all()
+    categories_list = [model_to_dict(category) for category in categories]
+    return categories_list
+
+
+def _get_poems_by_category_id(category_id):
+    """
+    根据类别id返回古诗
+    :return:
+    """
+    result = {'category_id': category_id}
+    # 类别信息
+    category = Categories.query.get(category_id)
+    result.update({'category_name': category.category_name})
+    # 诗词信息
+    category_poems = [{'second_level_category': category_poem.second_level_category,
+                       'poem': model_to_dict(Poems.query.get(category_poem.id))} for category_poem in
+                      CategoryPoem.query.filter_by(category_id=category_id).all()]
+    result.update({'poems': [{'second_level_category': category_poem.get('second_level_category'),
+                              'id': category_poem.get('poem').get('id'),
+                              'title': category_poem.get('poem').get('title'),
+                              'author_short': category_poem.get('poem').get('author_short')
+                              }
+                             for category_poem in category_poems
+                             ]
+                   })
+    return result
