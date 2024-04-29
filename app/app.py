361c2b6f2beb891891b8fc53f6aae7ed26996fc4
guokeyuan <guokeyuan@longfor.com>
2024-04-29 18:20:51 +0800

    优化app创建实例过程

--- app/app.py
+++ app/app.py
@@ -6,20 +6,25 @@
 # @File    : config.py
 # 功能描述  ：主应用入口
 import sys
-
 from flask import Flask
 from .config import Config
-from .config import db
 from .routes import routes
 
 sys.path.extend([r"/data/env/studyPoem-server/studypoem_server"])
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db.init_app(app)
-
-# 注册路由
-app.register_blueprint(routes)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)  # 加载配置
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
+
+    # 初始化数据库
+    from .models import db
+    db.init_app(app)
+
+    # 注册路由
+    app.register_blueprint(routes)
+
     app.run()
